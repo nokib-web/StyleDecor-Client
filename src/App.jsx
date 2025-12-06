8bddb613ca7f90d5901ff333f3c54382dff348f8
nokib-web <nokibnokib1@gmail.com>
2025-12-06 14:44:56 +0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div>
+      <h1 className='text-3xl font bold'>Hi i am nokib</h1>
+     </div>
+        
+       
+    </>
+  )
+}
+
+export default App
